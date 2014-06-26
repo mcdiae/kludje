77cb6caa6b4469091129b0a39687d8eeba416595
mcdiae <mcdiae@users.noreply.github.com>
2014-06-26 20:13:55 +0100

    test code

---
+++ kludje-testkit/src/main/java/uk/kludje/test/sample/ArbitraryInterfaces.java
@@ -0,0 +1,31 @@
+package uk.kludje.test.sample;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import static uk.kludje.fn.nary.UTriFunction.asUTriFunction;
+
+public class ArbitraryInterfaces {
+
+  public void invoke() {
+    call(asUTriFunction(this::download)::apply);
+  }
+
+  private boolean download(File target, URI source, Object ignored) throws IOException {
+    try (InputStream in = source.toURL().openStream()) {
+      return true;
+    }
+  }
+
+  private void call(Foo foo) {
+    foo.exec(new File("foo.txt"), URI.create("http://foo"), null);
+  }
+
+  public static interface Foo {
+    boolean exec(File a, URI b, Object x);
+  }
+}

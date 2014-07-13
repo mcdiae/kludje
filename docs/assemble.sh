c1c642d0c45ac5d0809f00661a5b7c3906e8d8f9
mcdiae <mcdiae@users.noreply.github.com>
2014-07-13 22:03:03 +0100

    0.2 documentation

--- docs/assemble.sh
+++ docs/assemble.sh
@@ -10,3 +10,10 @@ cd web/artefacts/api/0.1
 wget http://search.maven.org/remotecontent?filepath=uk/kludje/kludje-core/0.1/kludje-core-0.1-javadoc.jar -O docs.jar
 jar -xvf  docs.jar
 rm docs.jar
+cd ../../../..
+
+mkdir -p web/artefacts/api/0.2
+cd web/artefacts/api/0.2
+wget http://search.maven.org/remotecontent?filepath=uk/kludje/kludje-core/0.2/kludje-core-0.2-javadoc.jar -O docs.jar
+jar -xvf  docs.jar
+rm docs.jar

db11c9776c4fa0ab932b4d003046fae570f3db0c
mcdiae <mcdiae@users.noreply.github.com>
2014-07-13 22:27:33 +0100

    0.2 documentation

--- kludje-core/src/main/java/uk/kludje/fn/util/package-info.java
+++ kludje-core/src/main/java/uk/kludje/fn/util/package-info.java
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+/**
+ * Checked exception handling functional interfaces in {@code java.util}.
+ */
 @UncheckedFunctionalInterface(Comparator.class) package uk.kludje.fn.util;
 
 import uk.kludje.annotation.UncheckedFunctionalInterface;

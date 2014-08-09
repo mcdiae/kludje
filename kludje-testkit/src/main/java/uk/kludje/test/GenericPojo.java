0a7b78c2b138a2b2271123124625de1e64b88d49
mcdiae <mcdiae@users.noreply.github.com>
2014-08-09 17:09:13 +0100

    License info

--- kludje-testkit/src/main/java/uk/kludje/test/GenericPojo.java
+++ kludje-testkit/src/main/java/uk/kludje/test/GenericPojo.java
@@ -1,11 +1,35 @@
+/*
+ *
+ * Copyright $year McDowell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * /
+ */
+
 package uk.kludje.test;
 
 import uk.kludje.Meta;
+import uk.kludje.experimental.proxy.Interface;
+
+import java.util.function.Supplier;
 
 import static uk.kludje.Meta.meta;
 
 public class GenericPojo<T> {
-  private static final Meta<GenericPojo> META = meta(GenericPojo.class)
+  private final static Interface<GenericPojo<?>> iface = new Interface<GenericPojo<?>>() {
+  };
+  private final static Class<GenericPojo<?>> TYPE = GenericPojo.class.
+  private static final Meta<GenericPojo<?>> META = meta(iface.type())
       .objects($ -> $.gen);
 
   private final T gen;

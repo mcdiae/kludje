f3463c493e7c38188e55ab8a4b94e137c36062bd
mcdiae <mcdiae@users.noreply.github.com>
2015-03-07 13:13:47 +0000

    Docs

--- docs/assemble.sh
+++ docs/assemble.sh
@@ -5,30 +5,12 @@ rm -Rf web/artefacts/*
 mkdir -p web/artefacts/sample
 cp ../kludje-testkit/src/main/java/uk/kludje/test/sample/* web/artefacts/sample/
 
-mkdir -p web/artefacts/api/0.1
-cd web/artefacts/api/0.1
-wget http://search.maven.org/remotecontent?filepath=uk/kludje/kludje-core/0.1/kludje-core-0.1-javadoc.jar -O docs.jar
-jar -xvf  docs.jar
-rm docs.jar
-cd ../../../..
-
-mkdir -p web/artefacts/api/0.2
-cd web/artefacts/api/0.2
-wget http://search.maven.org/remotecontent?filepath=uk/kludje/kludje-core/0.2/kludje-core-0.2-javadoc.jar -O docs.jar
-jar -xvf  docs.jar
-rm docs.jar
-cd ../../../..
-
-mkdir -p web/artefacts/api/0.3
-cd web/artefacts/api/0.3
-wget http://search.maven.org/remotecontent?filepath=uk/kludje/kludje-core/0.3/kludje-core-0.3-javadoc.jar -O docs.jar
-jar -xvf  docs.jar
-rm docs.jar
-cd ../../../..
-
-mkdir -p web/artefacts/api/0.4
-cd web/artefacts/api/0.4
-wget http://search.maven.org/remotecontent?filepath=uk/kludje/kludje-core/0.4/kludje-core-0.4-javadoc.jar -O docs.jar
-jar -xvf  docs.jar
-rm docs.jar
-cd ../../../..
+for i in `seq 1 5`;
+do
+	mkdir -p web/artefacts/api/0.$i
+	cd web/artefacts/api/0.$i
+	wget http://search.maven.org/remotecontent?filepath=uk/kludje/kludje-core/0.$i/kludje-core-0.$i-javadoc.jar -O docs.jar
+	jar -xvf  docs.jar
+	rm docs.jar
+	cd ../../../..
+done

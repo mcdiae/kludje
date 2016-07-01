8df7b4f4c3a7f568aa299a9a79f4dd5fbf98a680
user <user@kludje.lan>
2016-07-01 20:33:29 +0100

    Meta changes

--- kludje-experimental/src/test/java/uk/kludje/experimental/test/meta/MetaTest.java
+++ kludje-experimental/src/test/java/uk/kludje/experimental/test/meta/MetaTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 McDowell
+ * Copyright 2016 McDowell
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
